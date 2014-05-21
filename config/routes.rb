7ded99efca0625d68dace90edb8a03dbf3b932d9
Huy <nguy1702@umn.edu>
2014-05-21 14:02:18 -0500

    Finish user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp2::Application.routes.draw do
- 
+  resources :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Huy Nguyen"
+    email    "Huy@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
