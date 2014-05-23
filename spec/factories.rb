ea68e0eecd4cc95b5602938759f1ff288ef43897
Huy <nguy1702@umn.edu>
2014-05-23 10:57:13 -0500

    Add user posts

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
     email    "Huy@example.com"
     password "foobar"
     password_confirmation "foobar"
+     factory :admin do
+      admin true
+    end
+  end
+  
+    factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
